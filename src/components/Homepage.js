f7d9a4ff1922174b976a756273cf758408b19fe7
unknown <paramjoshi27@gmail.com>
2023-04-17 04:40:18 +0530

    alttext

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -10,7 +10,7 @@ const Homepage = () => {
           {/* <div className='h-[100vh]' ></div> */}
           <div className='w-1/2 flex flex-col items-center relative top-[-150px]'>
           <h1 className="text-[80px] font-bold mb-8">Monitor</h1>
-          <img className='h-40' src='./images/logo.png'></img>
+          <img alt='logo' className='h-40' src='./images/logo.png'></img>
 
           </div>
     <div className='flex w-1/2 h-3/4 justify-center items-start' >
