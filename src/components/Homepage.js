ed462e642fd78d2c2644519e3380749e70fd06d4
unknown <paramjoshi27@gmail.com>
2023-04-17 05:09:43 +0530

    mobile

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -8,12 +8,13 @@ const Homepage = () => {
   return (
         <div className=" flex justify-center items-center h-screen bg-blue-200">
           {/* <div className='h-[100vh]' ></div> */}
-          <div className='w-1/2 flex flex-col items-center relative top-[-150px]'>
+          <div className='w-full sm:w-1/2 flex flex-col items-center relative top-[-150px]'>
           <h1 className="text-[80px] font-bold mb-8">Monitor</h1>
           <img alt='logo' className='h-40' src='./images/logo.png'></img>
+          <p className='sm:hidden mt-8 text-gray-600 text-[20px] w-full'>our website is not yet available on mobile-devices</p>
 
           </div>
-    <div className='flex w-1/2 h-3/4 justify-center items-start' >
+    <div className='hidden sm:flex w-1/2 h-3/4 justify-center items-start hide-in-mobile' >
     <Login/>
     <Signup/>
 
